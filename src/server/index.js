c4607a2d78775999a4b7467f2f9315c734bd65d2
tryiou <tryou@tryou.com>
2025-05-20 22:32:42 +0200

    feat: update electron version and comment out GPU switch in index.js

--- src/server/index.js
+++ src/server/index.js
@@ -38,9 +38,9 @@ if(foundArg && foundArg.match(debugArgPatt)[1] === 'true') {
 }
 
 // enforce electron launch param on linux
-if(process.platform === 'linux') {
-  app.commandLine.appendSwitch('--in-process-gpu');
-}
+// if(process.platform === 'linux') {
+//   app.commandLine.appendSwitch('--in-process-gpu');
+// }
 
 // Handle any uncaught exceptions
 process.on('uncaughtException', err => {
